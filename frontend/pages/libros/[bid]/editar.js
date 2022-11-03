a6c706358fde4b456dac082eeccfd56634742808
Juan David Castaño Gómez <juandcastag@gmail.com>
2022-11-03 06:03:22 -0500

    Install frontend y modules

---
+++ frontend/pages/libros/[bid]/editar.js
@@ -0,0 +1,75 @@
+import Link from 'next/link';
+import { useState } from 'react';
+import { useRouter } from 'next/router'
+
+
+export async function getServerSideProps({ params }) {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/books/${params.bid}`)
+    const data = await res.json()
+
+    return {
+        props: {
+            book: data
+        }
+    }
+}
+
+const BookEdit = ({ book }) => {
+    const router = useRouter()
+    const [bookTitle, setBookTitle] = useState(book.title)
+    const [errors, setErrors] = useState([])
+    const [submitting, setSubmitting] = useState(false)
+
+    async function handelSubmit(event) {
+        event.preventDefault()
+        setSubmitting(true)
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/books/${book.id}`, {
+            method: 'POST',
+            headers: {
+                accept: 'application/json',
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify({
+                title: bookTitle,
+                _method: 'PATCH'
+            })
+        })
+
+        if(res.ok) {
+            setErrors([])
+            setBookTitle('')
+            return router.push('/libros')
+        }
+            const data = await res.json()
+            setErrors(data.errors)
+            setSubmitting(false)
+        
+
+    }
+
+    return (
+        <>
+            <h1>BookEdit</h1>
+            <form onSubmit={handelSubmit}>
+                <input
+                    onChange={(e) => setBookTitle(e.target.value)}
+                    value={String(bookTitle)}
+                    disabled={submitting}
+                    type="text"
+                ></input>
+                <button
+                    disabled={submitting}
+                >{submitting ? 'Enviando...' : 'Enviar'}</button>
+                {errors.title && (
+                    <span style={{color: 'red', display: 'block'}}>{errors.title}</span>
+                )}
+            </form>
+            <br></br>
+            <Link href="/libros">Book List</Link>
+
+            {/* <p>{JSON.stringify(errors)}</p> */}
+        </>
+    )
+}
+
+export default BookEdit
---
+++ frontend/pages/libros/index.js
@@ -0,0 +1,72 @@
+import Link from 'next/link';
+
+export async function getStaticProps() {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/books`)
+    // const set = new Set(res);
+    
+    const data = await res.json()
+    console.log(data);
+
+    // const result = Array.from(set).map(element => element + 1);
+
+    return {
+        props: {
+            books: data.data
+        }
+    }
+}
+
+const BookList = ({ books }) => {
+    async function handleDelete(event, bookId) {
+        event.preventDefault()
+
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/books/${bookId}`, {
+            method: 'POST',
+            headers: {
+                accept: 'application/json',
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify({
+                _method: 'DELETE'
+            })
+        })
+
+        if(res.ok) {
+            window.location.href = '/libros'
+        }
+            const data = await res.json()
+            setErrors(data.errors)
+            setSubmitting(false)
+    }
+
+    return (
+        <div>
+            <pre>{JSON.stringify(books)}</pre>
+            <h1>Libros</h1>
+            <ul>
+                {books.map(book => (
+                    <li key={`book-${book.id}`}>
+                        <Link href={`/libros/${book.id}`}>
+                            {book.title}
+                        </Link>
+                        {' - '}
+                        <Link href={`/libros/${book.id}/editar`}>
+                            Editar
+                        </Link>
+                        {' - '}
+                        <form
+                            onSubmit={(e) => handleDelete(e, book.id)}
+                            style={{display: 'inline'}}
+                        >
+                            <button>Eliminar</button>
+                        </form>
+                    </li>
+                ))}
+            </ul>
+            <Link href="/libros/crear">Crear Libro</Link>
+            <p></p>
+        </div>
+    );
+};
+
+export default BookList;
